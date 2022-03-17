659bd818baf994f8c5646c5ae3d269716cf32860
eveliastico <betoestrada45@hotmail.com>
2022-03-17 12:43:48 -0700

    Primera vez

---
+++ build/generated-sources/ap-source-output/videojuegos_10am/entidades/Jugador_.java
@@ -0,0 +1,15 @@
+package videojuegos_10am.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-17T11:21:13")
+@StaticMetamodel(Jugador.class)
+public class Jugador_ { 
+
+    public static volatile SingularAttribute<Jugador, String> correo;
+    public static volatile SingularAttribute<Jugador, Long> id;
+    public static volatile SingularAttribute<Jugador, String> nombre;
+
+}
---
+++ src/videojuegos_10am/Videojuegos_10am.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videojuegos_10am;
+
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import interfaces.*;
+import implementaciones.*;
+import entidades.*;
+import enumeradores.Sexo;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author lv1013
+ */
+public class Videojuegos_10am {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IJugadoresDAO jugadoresDAO = DAOSFactory.crearJugadoresDAO();
+        Jugador jugador = new Jugador("juan", 32, new GregorianCalendar(), Sexo.FEMENINO);
+        jugadoresDAO.agregar(jugador);
+        
+        System.out.println(jugadoresDAO.consultarTodos());
+    }
+    
+}
